2384a422dfccf34cae97a3c2475c4ba6f310a9c9
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-26 06:11:05 +0600

    header compo add & header style added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Confusing Books</h1>
+    <div>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <h2>Confusing Books</h2>
+            <p>Choose any 4 books</p>
+        </nav>
+    );
+};
+
+export default Header;

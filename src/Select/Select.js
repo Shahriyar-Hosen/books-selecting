a68ec79aba1a58550ed0a9865e995724c607aa7a
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-26 21:56:27 +0600

    select cart img added

--- src/Select/Select.js
+++ src/Select/Select.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
+import './Select.css'
 
-const Select = ({book}) => {
-    const {name} = book;
-    return (
-        <div>
-            <h3>{name}</h3>
-        </div>
-    );
+const Select = ({ book }) => {
+  const { name, img } = book;
+  return (
+    <div className="select">
+      <img src={img} alt="" />
+      <h3>{name}</h3>
+    </div>
+  );
 };
 
 export default Select;
